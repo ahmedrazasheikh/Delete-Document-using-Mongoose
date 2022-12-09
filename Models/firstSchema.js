0cb47050575bbed486ce15223807a5096744427c
Ahmed Raza <bhaia9036@gmail.com>
2022-12-09 01:33:43 -0800

    initial commit

---
+++ Models/firstSchema.js
@@ -0,0 +1,106 @@
+import mongoose from "mongoose";
+
+const firstSchema = new mongoose.Schema({
+    name: { type: String, required: true, trim: true },
+    age: {
+        type: Number, required: true,
+        min: 18, max: 50
+    },
+
+    fees: {
+        type: mongoose.Decimal128, required: true, validate: v => v >= 5500.50
+    },
+    hobbies: { type: Array },
+    isActive: { type: Boolean },
+    comments: [{ value: { type: String }, publish: { type: Date, default: Date.now } }],
+    join: { type: Date, default: Date.now }
+
+})
+
+
+// Here You use the word members which createcollection with the name of Members//
+const firstSchemaSend = mongoose.model('member', firstSchema)
+
+
+// Update By ID 
+
+// const updateFunction =  (id) => {
+//     firstSchemaSend.findByIdAndUpdate(id, { name: 'Gourav' },
+//     function (err, docs) {
+// if (err){
+// console.log(err)
+// }
+// else{
+// console.log("Updated User : ", docs);
+// }
+// });
+// }
+
+// Updatae Single Document  
+// const updateFunction =  (id) => {
+//     firstSchemaSend.updateOne({_id : id}, { name: 'Ahmed Raza Jafri !!!!!!' },
+//     function (err, docs) {
+// if (err){
+// console.log(err)
+// }
+// else{
+// console.log("Updated User : ", docs);
+// }
+// });
+// }
+
+// Upsert do this thing if the id you give not exist it create the document
+
+// const updateFunction =  (id) => {
+//     firstSchemaSend.updateOne({_id : id}, { name: 'Muhammad Nadeem Hashmi' }, {upsert: true},
+//     function (err, docs) {
+// if (err){
+// console.log(err)
+// }
+// else{
+// console.log("Updated User : ", docs);
+// }
+// });
+// }
+
+// The First Value is Kind Of Filter
+
+
+// here i am give feeS in the position of id
+// const updateFunction =  (id) => {
+//     firstSchemaSend.updateOne({ name :id }, {name : 'Ahmed Raza Sunni'  }, {upsert: true},
+//     function (err, docs) {
+// if (err){
+// console.log(err)
+// }
+// else{
+// console.log("Updated User : ", docs);
+// }
+// });
+// }
+
+// Update Many Collections 
+const updateFunction =  (id) => {
+    firstSchemaSend.updateMany({ name :id }, {name : 'Ahmed Raza Sunni'  }, {upsert: true},
+    function (err, docs) {
+if (err){
+console.log(err)
+}
+else{
+console.log("Updated User : ", docs);
+}
+});
+}
+
+export default updateFunction;
+
+
+
+
+// If we want to about something than we use
+
+// console.log(firstSchema.path('age'))
+
+
+// Compiling Schema 
+
---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 8000
+import './Models/firstSchema.js'
+import updateFunction  from  './Models/firstSchema.js'
+// import familyMembers from './Models/firstSchema.js';
+import Database from './Database/dataBase.js'
+// import updateFunction from './Models/firstSchema.js'
+// const dataBase = process.env.dataBase || 
+// 'mongodb://hello_admin:hello123@0.0.0.0:27017/rtm?authSource=rtm'
+const dataBase = process.env.dataBase || 'mongodb://0.0.0.0:27017'
+Database(dataBase)
+updateFunction('MuhammadHassan')
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+})
